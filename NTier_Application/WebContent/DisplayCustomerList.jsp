4edd29085abbf1f26b6b8cda55e56ebb3be7da2b
Ishika Banerjee <ishikabanerjee100@gmail.com>
2016-03-10 15:12:43 +0530

    N Tier Application

---
+++ NTier_Application/WebContent/DisplayCustomerList.jsp
@@ -0,0 +1,16 @@
+<%@ page import="java.util.*, com.training.entity.Customer" language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1><a href="ShowCustomer" >Customer List</a></h1>
+<% List<Customer> customerList = (List<Customer>) request.getAttribute("CustomerList"); 
+	
+		out.println(customerList);
+	%>  
+</body>
+</html>
---
+++ NTier_Application/src/com/training/servlet/ShowCustomer.java
@@ -0,0 +1,51 @@
+package com.training.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.training.daos.CustomerDAO;
+import com.training.entity.Customer;
+
+/**
+ * Servlet implementation class ShowCustomer
+ */
+public class ShowCustomer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowCustomer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CustomerDAO dao = new CustomerDAO();
+        List<Customer> customerList = dao.findAll();
+		
+		request.setAttribute("CustomerList", customerList);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("DisplayCustomerList.jsp");
+		
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
