fa7955a3b7880c5e7c04257273657188418ae6ba
Ishika Banerjee <ishikabanerjee100@gmail.com>
2016-03-16 13:37:32 +0530

    Modified

--- RESTfulServices/src/main/java/com/training/ResTfulServicesApplication.java
+++ RESTfulServices/src/main/java/com/training/ResTfulServicesApplication.java
@@ -11,8 +11,8 @@ public class ResTfulServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResTfulServicesApplication.class, args);
-//		RestTemplate restTemplate = new RestTemplate();
-//		String obj = restTemplate.getForObject("http://localhost:6060/person/all", String.class);
-//		System.out.println(obj);
+		RestTemplate restTemplate = new RestTemplate();
+		String obj = restTemplate.getForObject("http://localhost:6060/person/all", String.class);
+		System.out.println(obj);
 	}
 }
