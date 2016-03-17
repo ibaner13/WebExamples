f31122b13555e27b9229c0a383ff32ef93a4d068
Ishika Banerjee <ishikabanerjee100@gmail.com>
2016-03-17 15:47:41 +0530

    Gaurav

---
+++ MVCBootApp/src/main/java/com/training/MvcBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBootAppApplication.class, args);
+	}
+}
